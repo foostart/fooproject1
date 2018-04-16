c8e454d7ab322b599b47739bd05990efda3daa12
PhanThanhTu <tuty.kara@gmail.com>
2018-04-16 16:36:30 +0700

    up 1230
    up

---
+++ design/blocks/1230/1230-content.php
@@ -0,0 +1,148 @@
+<?php
+$url_host = 'http://'.$_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-1230">
+    <div class="container">
+        <div class="row">
+            <div class="content">
+                <div class="col-md-12">
+                    <div class="blog-top">
+                        <h4>Giới thiệu đặc trưng</h4>
+                    </div>
+                </div>
+                <div class=" clearfix"></div>
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                      <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/1.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Sang trọng, sang trọng và sắc nét</a></h2>
+                             <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                        
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/2.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Xưởng đóng tàu Yanicke Taichung</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/3.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Sang trọng, sang trọng và sắc nét</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/4.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Công nghệ Liwei</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/5.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Mẫu tuyệt vời, phong cách cuộc sống</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/6.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Văn phòng Thành phố Mới Đài Bắc</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/7.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Công nghệ Liwei</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/8.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Mẫu tuyệt vời, phong cách cuộc sống</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/9.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Công nghệ Liwei</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide col-md-3 col-sm-3 col-xs-12">
+                        <div class="blog-content">
+                            <div class="blog-img">
+                                <a href="#"><img src="<?php echo $url_path ?>/images/10.jpg" alt=""/></a>
+                            </div>
+                            <div class="portfolio-overlay">
+                            <a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
+                            </div>
+                            <h2 class="blog-title"><a href="#">Văn phòng Thành phố Mới Đài Bắc</a></h2>
+                            <p class="blog-description"><a href="#">Thành phố Đài Bắc</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
---
+++ design/blocks/1230/1230.php
@@ -0,0 +1,74 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('less/1230.less', 'css/1230.css');
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">       
+        <link href="<?php echo $url_path ?>/css/1230.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo $url_path ?>/css/swiper.min.css" rel="stylesheet" type="text/css"/> 
+        <script src="<?php echo $url_path ?>/js/swiper.min.js"></script>   
+        <title>type-1230</title>
+    </head>
+
+    <body>
+        <?php
+            include '1230-content.php';
+        ?>
+        <script>
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      slidesPerGroup: 2,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+   
+    breakpoints: {
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+      },
+      960: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+      },
+      480: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+      },
+
+  } 
+
+});
+</script>
+    </body>
+
+</html>
+
+
+
+
