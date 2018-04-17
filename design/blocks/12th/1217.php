82fbb593fb3082f1f06c9557fc44d3895b5d4594
Hoan <hoan5201@gmail.com>
2018-04-17 16:39:35 +0700

    cm

--- design/blocks/12th/1217.php
+++ design/blocks/12th/1217.php
@@ -14,25 +14,39 @@
     }
     $less = new lessc;
     $less->compileFile('less/1214.less', 'css/1214.css');
+    $less = new lessc;
+    $less->compileFile('less/1217.less', 'css/type-1217.css');
+    
     
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
     <head>
-        <title>1214</title>
-        
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">       
         <link href="css/1214.css" rel="stylesheet" type="text/css"/>
         <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/1214.js" type="text/javascript"></script>
-        
+        <link href="<?php echo $url_path ?>/css/type-1217.css" rel="stylesheet" type="text/css" /> 
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet">   
+        <title>1217</title>
     </head>
 
     <body>
-        <?php include $dir_block.'/1214-content.php'; ?>
+        <?php
+            include $dir_block. '/1214-content.php';
+            include $dir_block. '/1217-content.php';
+        ?>
+       
+     
     </body>
+
 </html>
+
+
+
+
