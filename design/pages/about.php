d664198052720de1e99a1a93e65e2a69fb92c345
minhphuc1909 <huynhminhphuc1909@gmail.com>
2018-04-18 09:45:46 +0700

    page-about

---
+++ design/pages/about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="vi">
+    <head>
+        <title>Home page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=100" />
+        <meta http-equiv="REFRESH" content="1800" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Tai lieu web" />
+        <meta name="description" content="Tai lieu web" />
+        <meta name="keywords" content="Tai lieu web"/>
+
+        <link rel="shortcut icon" href="blocks/2034/images/favicon.ico"/>
+
+        <link href="../blocks/1214/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../blocks/1214/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+
+        <link href="../blocks/1218/css/1218.css" rel="stylesheet" type="text/css"/>
+        <link href="../blocks/1219/css/type-1219.css" rel="stylesheet" type="text/css"/>
+         <link href="../blocks/1220/css/1220.css" rel="stylesheet" type="text/css" /> 
+         <link href="../blocks/1221/css/1221.css" rel="stylesheet" type="text/css" />
+         <link href="../blocks/1222/css/type-1222.css" rel="stylesheet" type="text/css"/>
+         <link href="../blocks/1223/css/type-1223.css" rel="stylesheet" type="text/css"/> 
+        
+        
+        <script src="../blocks/1214/js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="../blocks/1215/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="../blocks/1215/js/swiper.min.js" type="text/javascript"></script>
+        <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.12';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    </head>
+    <body>
+
+        <?php  include '../blocks/1218/1218-content.php'; ?>
+        <?php  include '../blocks/1219/1219-content.php'; ?>
+        <?php  include '../blocks/1220/1220-content.php'; ?>
+        <?php  include '../blocks/1221/1221-content.php'; ?>
+        <?php  include '../blocks/1222/1222-content.php'; ?>
+        <?php  include '../blocks/1223/1223-content.php'; ?>
+
+    </body>
+</html>
