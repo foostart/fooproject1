112b62285aab8b0ff2eb888124a7a0cfe8fbf692
PhanThanhTu <tuty.kara@gmail.com>
2018-04-19 10:36:41 +0700

    up

--- design/blocks/1230/1230.php
+++ design/blocks/1230/1230.php
@@ -35,45 +35,6 @@
         <?php
             include '1230-content.php';
         ?>
-        <script>
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-   nextButton: '#swiper_btn_next',
-    prevButton: '#swiper_btn_prev',
-    breakpoints: {
-        
-      1200: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-      },
-      990: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-      },
-      800: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-      },
-
-
-      770: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-            },
-            
-
-        } 
-
-      });
-      </script>
     </body>
 </html>
 
