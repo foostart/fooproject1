2bcba7d5c2dcb3d7a07fd2e550731631c3641610
minhphuc1909 <huynhminhphuc1909@gmail.com>
2018-05-02 09:43:36 +0700

    page About

--- design/pages/About.php
+++ design/pages/About.php
@@ -31,9 +31,8 @@
         
         <script src="../blocks/1214/js/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="../blocks/1214/js/1214.js" type="text/javascript"></script>
-        <script src="../blocks/1215/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="../blocks/1215/js/swiper.min.js" type="text/javascript"></script>
-        
+        <script src="../blocks/1214/js/bootstrap.min.js" type="text/javascript"></script>
+       
     </head>
     <body>
         <?php  include '../blocks/1214/1214-1-content.php'; ?>
