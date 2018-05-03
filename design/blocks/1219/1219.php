6867010a39772b76b7c5dc3fdc964e3ddaec9da3
vucd1785 <36876695+vucd1785@users.noreply.github.com>
2018-05-03 09:20:10 +0700

    cm

--- design/blocks/1219/1219.php
+++ design/blocks/1219/1219.php
@@ -1,20 +1,21 @@
 <?php
+    include '../config.php';
+    if (empty($url_path)) {
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
-$url_host = 'http://'.$_SERVER['HTTP_HOST'];
-$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
-$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-
-preg_match_all($pattern_uri, __DIR__, $matches);
-$url_path = $url_host . $matches[1][0];
-$url_path = str_replace('\\', '/', $url_path);
-
-if (!class_exists('lessc')) {
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+}
+    if (!class_exists('lessc')) {
     $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
     require_once($dir_block.'/libs/lessc.inc.php');
 }
 
-$less = new lessc;
-$less->compileFile('less/1219.less', 'css/1219.css');
+    $less = new lessc;
+    $less->compileFile('less/1219.less', 'css/1219.css');
 
 ?>
 <!DOCTYPE html>
@@ -38,7 +39,3 @@ $less->compileFile('less/1219.less', 'css/1219.css');
 </body>
 
 </html>
-
-
-
-
