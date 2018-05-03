0ee1be68f34bcab4982f4d6b5e132dde2c394238
foostart.com <foostart.com@gmail.com>
2018-05-03 08:16:00 +0700

    27 28 29 error structure page

--- design/blocks/1228/1228.php
+++ design/blocks/1228/1228.php
@@ -4,31 +4,31 @@
     $url_host = 'http://'.$_SERVER['HTTP_HOST'];
     $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
     $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-    
+
     preg_match_all($pattern_uri, __DIR__, $matches);
     $url_path = $url_host . $matches[1][0];
     $url_path = str_replace('\\', '/', $url_path);
 
     if (!class_exists('lessc')) {
-        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
         require_once($dir_block.'/libs/lessc.inc.php');
     }
-    
+
     $less = new lessc;
   $less->compileFile('less//1228.less', 'css/1228.css');
-    
+
 ?>
 <html lang="en-US">
 <head>
     <meta charset="UTF-8">
     <title>modul 1228</title>
-   
+
     <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet">       
+    <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?php echo $url_path ?>/css/1228.css" rel="stylesheet" type="text/css" />
     <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js" type="text/javascript"></script>
-        <script src="<?php echo $url_path ?>/js/script.js" type="text/javascript"></script>
-   
+        <script src="<?php echo $url_path ?>/js/1228.js" type="text/javascript"></script>
+
 </head>
 
 <body>
