200d29143fcaeacd11d970696c524e5dde35946f
LEPHUOCNGUYEN <33856656+ThienHaPhiNguyen@users.noreply.github.com>
2018-05-03 09:40:08 +0700

    Sua module 1223.
    Xoa file rac, them if va sua file team.

--- design/blocks/1223/1223.php
+++ design/blocks/1223/1223.php
@@ -1,5 +1,6 @@
 <?php
-
+include '../config.php';
+if (empty($url_path)) {
     $url_host = 'http://'.$_SERVER['HTTP_HOST'];
     $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
     $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
@@ -7,12 +8,12 @@
     preg_match_all($pattern_uri, __DIR__, $matches);
     $url_path = $url_host . $matches[1][0];
     $url_path = str_replace('\\', '/', $url_path);
+}
 
     if (!class_exists('lessc')) {
         $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
         require_once($dir_block.'/libs/lessc.inc.php');
     }
-    
     $less = new lessc;
     $less->compileFile('less/1223.less', 'css/1223.css');
     
