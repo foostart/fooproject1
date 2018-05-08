0712318fa760cc4ee5a2c4daf7529e524b9a439a
letathung1996 <pecottu111@gmail.com>
2018-05-08 22:00:02 +0700

    commit

--- design/blocks/1219/1219-content.php
+++ design/blocks/1219/1219-content.php
@@ -12,87 +12,87 @@
 <div class="type-1219">
   <div class="container">
     <div class="row">
-         <div class="in">COMPANY ORGANIZATION
-              <span class="depa">BRANCH</span>
+         <div class="in">TỔ CHỨC CÔNG TY
+              <span class="depa">CHI NHÁNH</span>
           </div>
          <div class="boder"></div>
        <a href="#" class=" button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Daan Company</div>
+           <div class="text">Công ty Daan</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Chengde company</div>
+           <div class="text">Công ty Chengde</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Revival One</div>
+           <div class="text">Công ty DELTA</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Revival II</div>
+           <div class="text">Công ty DELTA II</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Zhongxiao Company</div>
+           <div class="text">Công ty Zhongxiao</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Minsheng Company</div>
+           <div class="text">Công ty Minsheng</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Zhongshan Company</div>
+           <div class="text">Công ty Trung Sơn</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">South West Company</div>
+           <div class="text">Công ty Tây Nam</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Nanjing company</div>
+           <div class="text">Công ty Nam SA</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">Dunbei Company</div>
+           <div class="text">Công ty Dunbei</div>
        </a>
        <a href="#" class="button hvr-backward">
           <i class="fa fa-arrow-right ican"></i>
-           <div class="text">General Management Office</div>
+           <div class="text">Tổng cục quản lý</div>
        </a>
    </div>
  </div>
  <hr class="margin-tb-s">
  <div class="container">
    <div class="col-md-12">
-      <div class="in">COMPANY ORGANIZATION
-        <span class="depa">DEPARTMENT</span>
+      <div class="in">TỔ CHỨC CÔNG TY
+        <span class="depa">PHÒNG BAN</span>
       </div>
        <div class="boder"></div>
 
      <div class="col-md-2 col-sm-2">
        <i class="fa fa-lightbulb-o icon"></i><br>
-       <h3>DESIGN DEPARTMENT</h3>
+       <h3>BỘ PHẬN THIẾT KẾ</h3>
      </div>
      <div class="col-md-2 col-sm-2">
        <i class="fa fa-wrench icon"></i><br>
-       <h3>ENGINEERING DEPARTMENT</h3>
+       <h3>BỘ PHẬN KỸ THUẬT</h3>
      </div>
      <div class="col-md-2 col-sm-2">
        <i class="fa fa-phone icon"></i><br>
-       <h3>BULEGAL COUNSEL</h3>
+       <h3>TƯ VẤN TẠI CHỖ</h3>
      </div>
      <div class="col-md-2 col-sm-2">
        <i class="fa fa-briefcase icon"></i><br>
-       <h3>LEGAL COUNSEL</h3>
+       <h3>TƯ VẤN PHÁP LÝ</h3>
      </div>
      <div class="col-md-2 col-sm-2">
        <i class="fa fa-pencil-square-o icon"></i><br>
-       <h3>ACCOUNTING DEPARTMENT</h3>
+       <h3>PHÒNG KẾ TOÁN</h3>
      </div>
      <div class="col-md-2 col-sm-2">
        <i class="fa fa-usd icon"></i><br>
-       <h3>VALUATION DEPARTMENT</h3>
+       <h3>GIÁ TRỊ GIÁ TRỊ</h3>
      </div>
    </div>
  </div>
