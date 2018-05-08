c43bf4bc5f81d004885684c51da048a5b5354432
vucd1785 <36876695+vucd1785@users.noreply.github.com>
2018-05-08 14:46:12 +0700

    việt hóa 1217

--- design/blocks/1217/1217-content.php
+++ design/blocks/1217/1217-content.php
@@ -1,13 +1,13 @@
 <?php
- include '../config.php';
- 
-        $url_host = 'http://'.$_SERVER['HTTP_HOST'];
-        $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), $sala);
-        $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-        
-        preg_match_all($pattern_uri, __DIR__, $matches);
-        $url_path = $url_host . $matches[1][0];
-        $url_path = str_replace('\\', '/', $url_path);
+include '../config.php';
+if (empty($url_path)) {
+	$url_host = 'http://'.$_SERVER['HTTP_HOST'];
+	$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+	$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+	preg_match_all($pattern_uri, __DIR__, $matches);
+	$url_path = $url_host . $matches[1][0];
+	$url_path = str_replace('\\', '/', $url_path);
+}
 ?>
 <div class="type-1217">
     
@@ -16,7 +16,7 @@
             <div class="contact_us">
                 <div class="contact_title"> 
                     <img src="<?php echo $url_path ?>/images/contactus-title.png" alt= "img1">
-                    <div class="content_sub">The information you leave behind and the content of the message will be kept confidential. Please be sure to leave your contact details to ensure that our reply can be provided for your reference.</div>
+                    <div class="content_sub">Thông tin và nội dung tin nhắn của bạn sẽ được bảo mật. Hãy chắc chắn thông tin liên lạc mà bạn để lại để đảm bảo phản hồi của chúng tôi có thể hỗ trợ cho bạn .</div>
                 </div>
                 <!--Form Information-->
                 <div class="contact_form">
@@ -24,14 +24,14 @@
                         <div class="row">
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <label class="input">
-                                    <input type="text" name="name_1" placeholder="Contact person">
+                                    <input type="text" name="name_1" placeholder="Người liên lạc">
                                 </label>
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <select class="select" name="select_name">
-                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Gender</option>
-                                    <option value="#" class="option_select">Male</option>
-                                    <option value="#" class="option_select">Female</option>
+                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Giới Tính</option>
+                                    <option value="#" class="option_select">Nam</option>
+                                    <option value="#" class="option_select">Nữ</option>
                                     <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
                                 </select>
@@ -40,17 +40,17 @@
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <label class="input">
-                                    <input type="text" name="name_1" placeholder="Cell phone">
+                                    <input type="text" name="name_1" placeholder="Di Động">
                                 </label>
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <label class="input">
-                                    <input type="text" name="name_1" placeholder="Contact phone number">
+                                    <input type="text" name="name_1" placeholder="Số Điện Thoại Liên Lạc">
                                 </label>
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <label class="input">
-                                    <input type="text" name="name_1" placeholder="Address">
+                                    <input type="text" name="name_1" placeholder="Địa Chỉ">
                                 </label>
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
@@ -60,60 +60,60 @@
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <select class="select" name="select_name">
-                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Space category</option>
-                                    <option value="#" class="option_select">Residential</option>
-                                    <option value="#" class="option_select">Commercial space</option>
-                                    <option value="#" class="option_select">Office</option>
-                                    <option value="#" class="option_select">Old house renovation</option>
+                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Không gian sống</option>
+                                    <option value="#" class="option_select">Biệt thự</option>
+                                    <option value="#" class="option_select">Chung cư</option>
+                                    <option value="#" class="option_select">Công sở</option>
+                                    <option value="#" class="option_select">Nhà cổ điển</option>
                                 </select>
                                 <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <select class="select" name="select_name">
-                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Family member</option>
-                                    <option value="#" class="option_select">Singleton</option>
-                                    <option value="#" class="option_select">Couples</option>
-                                    <option value="#" class="option_select">Two generations together</option>
-                                    <option value="#" class="option_select">Three generations</option>
-                                    <option value="#" class="option_select">Many people live together</option>
+                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Số lượng thành viên</option>
+                                    <option value="#" class="option_select">Độc thân</option>
+                                    <option value="#" class="option_select">Cặp đôi</option>
+                                    <option value="#" class="option_select">Hai thế hệ</option>
+                                    <option value="#" class="option_select">Ba thế hệ</option>
+                                    <option value="#" class="option_select">Tập thể</option>
                                 </select>
                                 <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
                             </div>
                             <div class="col-md-4 col-sm-6 padding-20">
                                 <select class="select" name="select_name">
-                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Custom budget</option>
-                                    <option value="#" class="option_select">100 to 1.5 million</option>
-                                    <option value="#" class="option_select">150 to 2 million</option>
-                                    <option value="#" class="option_select">200 to 5 million</option>
-                                    <option value="#" class="option_select">500 to 10 million</option>
-                                    <option value="#" class="option_select">More than 10 million</option>
+                                    <option value="#" class="selected_disable" selected="true" disabled="disabled">Ngân sách dự kiến</option>
+                                    <option value="#" class="option_select">Dưới 150 triệu</option>
+                                    <option value="#" class="option_select">150-300 triệu</option>
+                                    <option value="#" class="option_select">300-500 triệu</option>
+                                    <option value="#" class="option_select">500 triệu-1 tỷ</option>
+                                    <option value="#" class="option_select">Hơn 1 tỷ</option>
                                 </select>
                                 <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
                             </div>
                             <div class="typehouse col-md-12 col-sm-12">
-                                <h4>Indoor pattern</h4>
+                                <h4>Kiến trúc trong nhà</h4>
                                 <div class="col-md-3 col-sm-6 padding-20">
                                     <select class="select" name="select_detail">
-                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Ping number</option>
-                                        <option value="#" class="option_select">30 pings or less</option>
-                                        <option value="#" class="option_select">30~40 pings</option>
-                                        <option value="#" class="option_select">40 to 60 pings</option>
-                                        <option value="#" class="option_select">60-100 pings</option>
+                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Số tầng</option>
+                                        <option value="#" class="option_select">1 tầng</option>
+                                        <option value="#" class="option_select">2 tầng</option>
+                                        <option value="#" class="option_select">3 tầng</option>
+                                        <option value="#" class="option_select">4 tầng</option>
                                     </select>
                                     <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
                                 </div>
                                 <div class="col-md-3 col-sm-6 padding-20">
                                     <select class="select" name="select_detail">
-                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Number of rooms</option>
-                                        <option value="#" class="option_select">1 room</option>
-                                        <option value="#" class="option_select">2 room</option>
-                                        <option value="#" class="option_select">3 room</option>
-                                        <option value="#" class="option_select">4 room</option>
-                                        <option value="#" class="option_select">5 room</option>
+                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Số lượng phòng</option>
+                                        <option value="#" class="option_select">1 phòng</option>
+                                        <option value="#" class="option_select">2 phòng</option>
+                                        <option value="#" class="option_select">3 phòng</option>
+                                        <option value="#" class="option_select">4 phòng</option>
+                                        <option value="#" class="option_select">5 phòng</option>
                                     </select>
                                     <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
@@ -121,33 +121,33 @@
                                 
                                 <div class="col-md-3 col-sm-6 padding-20">
                                     <select class="select" name="select_detail">
-                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Number of halls</option>
-                                        <option value="#" class="option_select">1 hall</option>
-                                        <option value="#" class="option_select">2 hall</option>
-                                        <option value="#" class="option_select">3 hall</option>
-                                        <option value="#" class="option_select">4 hall</option>
-                                        <option value="#" class="option_select">4 or more halls</option>
+                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Bảo vệ</option>
+                                        <option value="#" class="option_select">1 Người</option>
+                                        <option value="#" class="option_select">2 Người</option>
+                                        <option value="#" class="option_select">3 Người</option>
+                                        <option value="#" class="option_select">4 Người</option>
+                                        <option value="#" class="option_select">nhiều hơn 4 Người</option>
                                     </select>
                                     <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
                                 </div>
                                 <div class="col-md-3 col-sm-6 padding-20">
                                     <select class="select" name="select_detail">
-                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Wei number</option>
-                                        <option value="#" class="option_select">1 Wei</option>
-                                        <option value="#" class="option_select">2 Wei</option>
-                                        <option value="#" class="option_select">3 Wei</option>
-                                        <option value="#" class="option_select">4 Wei</option>
-                                        <option value="#" class="option_select">More than 4 guards</option>
+                                        <option value="#" class="selected_disable" selected="true" disabled="disabled">Số lượng hội trường</option>
+                                        <option value="#" class="option_select">1</option>
+                                        <option value="#" class="option_select">2</option>
+                                        <option value="#" class="option_select">3</option>
+                                        <option value="#" class="option_select">4</option>
+                                        <option value="#" class="option_select">Nhiều hơn 4</option>
                                     </select>
                                     <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                     <i class="fa fa-sort-up" aria-hidden="true"></i>
                                 </div>
                                 <div class="col-md-12 padding-20">
-                                    <textarea rows="5" class="detail" placeholder="Please leave your message" ></textarea>    
+                                    <textarea rows="5" class="detail" placeholder="Vui lòng để lại lời nhắn của bạn" ></textarea>    
                                 </div>
                                 <div class="col-md-12 padding-20">
-                                    <button type="submit" class="button">Send form</button>   
+                                    <button type="submit" class="button">Gửi</button>   
                                 </div>
                             </div>
                         </div>
