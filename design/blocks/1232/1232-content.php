8aaac6111840b108424542714f219c14e97565fb
vucd1785 <36876695+vucd1785@users.noreply.github.com>
2018-05-08 15:48:55 +0700

    viet hoa 1214,1232

--- design/blocks/1232/1232-content.php
+++ design/blocks/1232/1232-content.php
@@ -1,24 +1,22 @@
 <?php
- include '../config.php';
- 
-        $url_host = 'http://'.$_SERVER['HTTP_HOST'];
-        $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), $sala);
-        $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-        
-        preg_match_all($pattern_uri, __DIR__, $matches);
-        $url_path = $url_host . $matches[1][0];
-        $url_path = str_replace('\\', '/', $url_path);
+include '../config.php';
+$url_host = 'http://'.$_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), $sala);
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
 ?>
-<div class="type-1214">
+<div class="type-1232">
     <header>
         <!--MENU-->
         <div class="navbar navbar-default navbar-fixed-top navbar-1" id="menu-home">
-            <div class="container width-1214">
+            <div class="container width-1232">
                 <!--LOGO-->
                 <div class="logo" id="demo">
                     <a href="#">
                         <img class="img-logo" src="<?php echo $url_path ?>/images/logo.png"
-                             alt=""/>
+                        alt=""/>
                     </a>
                 </div>
                 <!--/END LOGO-->
@@ -31,18 +29,18 @@
                             <div class="search-bar"><i class="fa fa-search"></i></div>
                             <div class="search-box">
                                 <form class="search-form" id="main-search" name="main-search" action="#" method="post">
-                
-                <ul>
-                    <li>
-                        <label class="input">
-                            <input name="schtxt" placeholder="Search..." type="text">
-                        </label>
-                    </li>
-                    <li>
-                        <a href="#" type="submit" value="submit" id="qschbtn"><i class="fa fa-search bts"></i></a>
-                    </li>
-                </ul>
-            </form>
+
+                                    <ul>
+                                        <li>
+                                            <label class="input">
+                                                <input name="schtxt" placeholder="Search..." type="text">
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <a href="#" type="submit" value="submit" id="qschbtn"><i class="fa fa-search bts"></i></a>
+                                        </li>
+                                    </ul>
+                                </form>
                             </div>
                         </li>
                         <!--/END SEARCH-->
@@ -52,62 +50,58 @@
 
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed btn-menu" data-toggle="collapse"
-                            data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
+                    data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <!--MENU DROP DOWN-->
-                        <li class="dropdown show-hover">
-                            <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Company Profile<i class="fa fa-angle-down"></i></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">about chengshe</a></li>
-                                <li><a href="#">company organization</a></li>
-                                <li><a href="#">workflow</a></li>
-                                <li><a href="#">charges</a></li>
-                                <li><a href="#">case performance</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown show-hover">
-                            <a href="#" class="dropdown-toggle active" data-toggle="dropdown">work overview<i class="fa fa-angle-down"></i></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">residential space</a></li>
-                                <li><a href="#">comercial space</a></li>
-                                <li><a href="#">office space</a></li>
-                                <li><a href="#">charges</a></li>         
-                            </ul>
-                        </li>
-                        <li class="dropdown show-hover">
-                            <a href="#" class="dropdown-toggle active" data-toggle="dropdown">design team<i class="fa fa-angle-down"></i></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">daan comany</a></li>
-                                <li><a href="#">chengde company</a></li>
-                                <li><a href="#">revival one</a></li>
-                                <li><a href="#">revival II</a></li>
-                                <li><a href="#">zhongxiao company</a></li>
-                                <li><a href="#">minsheng company</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown show-hover">
-                            <a href="#" class="dropdown-toggle active" data-toggle="dropdown">customer service<i class="fa fa-angle-down"></i></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">online consultation</a></li>
-                                <li><a href="#">customer service</a></li>
-                            </ul>
-                        </li>
-                        <!--/END MENU DROP DOWN-->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <!--MENU DROP DOWN-->
+                    <li class="dropdown show-hover">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Giới thiệu công ty<i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Về công ty</a></li>
+                            <li><a href="#">Tổ chức công ty</a></li>
+                            <li><a href="#">Quy trình làm việc</a></li>
+                            <li><a href="#">Chi phí</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown show-hover">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Tổng quan công việc<i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Không gian dân cư</a></li>
+                            <li><a href="#">Không gian công sở</a></li>
+                            <li><a href="#">Chi phí</a></li>         
+                        </ul>
+                    </li>
+                    <li class="dropdown show-hover">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Đội ngũ thiết kế<i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">công ty Hòa Bình</a></li>
+                            <li><a href="#">Công ty An Phong</a></li>
+                            <li><a href="#">Công ty Central</a></li>
+                            <li><a href="#">Công ty Bảo Sơn</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown show-hover">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Hỗ trợ khách hàng<i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Tư vấn trực tuyến</a></li>
+                            <li><a href="#">Dịch Vụ</a></li>
+                        </ul>
+                    </li>
+                    <!--/END MENU DROP DOWN-->
 
-                        <!--MENU DROP DOWN-->
-    
-                        <!--/END MENU DROP DOWN-->
+                    <!--MENU DROP DOWN-->
 
-                        <!--MENU DROP DOWN-->
-                        
+                    <!--/END MENU DROP DOWN-->
+
+                    <!--MENU DROP DOWN-->
+
                         <!--/END MENU DR
                     </ul>
                 </div><!-- /.navbar-collapse -->
